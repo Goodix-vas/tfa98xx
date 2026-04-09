cccc4bc7b75f7c0fe13f63ea9ca65f229f7c7293
audio-jenkin <audio-jenkin>
2026-04-09 19:03:23 +0800

    Few updates for supporting new firmware for TFA

--- inc/tfa_service.h
+++ inc/tfa_service.h
@@ -33,7 +33,7 @@ extern "C" {
 #ifdef TFA98XX_GIT_VERSIONS
   #define TFA98XX_API_REV_STR TFA98XX_GIT_VERSIONS
 #else
-  #define TFA98XX_API_REV_STR "v6.17.4"
+  #define TFA98XX_API_REV_STR "v6.17.5"
 #endif
 
 #include "tfa_device.h"
@@ -41,7 +41,7 @@ extern "C" {
 /*
  * data previously defined in Tfa9888_dsp.h
  */
-#define MEMTRACK_MAX_WORDS           283
+#define MEMTRACK_MAX_WORDS           297
 #define LSMODEL_MAX_WORDS            150
 #define TFA98XX_MAXTAG              (150)
 #define FW_VAR_API_VERSION          (521)
